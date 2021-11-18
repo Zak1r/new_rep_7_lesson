a4c98b11cf145000060b1b13400bcdd74ac532bd
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-18 23:42:42 +0600

    till end of the lesson

---
+++ homework_4.py
@@ -0,0 +1,8 @@
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+# if num1 > num2: print(f'{num2} - min number')
+# else: print(f'{num2} - min number')
+
+num = int(input('Enter number: '))
+if num > 0: print(num **2)
+elif num < 0: print(num **(1/3))
---
+++ index.py
@@ -0,0 +1,62 @@
+#списки
+#12 список имен в 12 переменных 
+
+
+# print(names[1]) # print (ages[1]) # print(names[-3]) # print(ages[-3])
+# names = ['Bobur' , 'Zakir' , 'Malik' , 'Otabek']
+# ages = [17, 20, 28, 19]
+# names.append ('Elbek')         #вставить_еще_один_список = списки.append
+# ages.append(23)
+# names.append(True)
+
+# names.pop(-1)               #список.pop удаляет выбранную списку по индексу
+# ages.pop(-1)
+
+# print(names.pop(0))
+# print(ages.pop(0))
+
+# print(names)
+# print(ages)
+# # print(names[4])
+
+# names = ['Bobur' , 'Zakir' , 'Malik' , 'Otabek']
+# ages = [17, 20, 28, 19]
+
+# # names.insert(0, 'Elbek')            #список.insert добавляет о позицию
+# # ages.insert(0, 27)
+
+# #print(len(names))                   #len общая количество список
+
+# #names.remove('Otabek')              #список.remove удаление по значению
+# #ages.remove(19)
+
+# print(names) 
+# print(ages)
+
+
+
+# names = ['Bobur' , 'Zakir' , 'Malik' , 'Otabek' , 'Aziz' , 'Shukhrat' , 'Elbek' , 'Zafar']
+# ages = [17, 20, 28, 19]
+# print(len(names))
+
+# # names.reverse()                         #список.reverse  переварачивает список (переварачивает исходный массив)
+# # ages.reverse()
+
+# # names = names[::-1]
+# # print(names[::-1])                   #(список[::-1]) создает новый массив
+
+# print(names[::-1])                # [с какого начать (start): до какого (end)(необязательно писать сколько в списке просто оставить пустое место) : шаг - сколько пропустить(step)]
+
+
+# # clock value
+# hrs = int (input('Enter hours: '))
+# mins = int (input('Enter mins: '))
+# delta = int (input('Enter delta hours: '))
+# if hrs >= 0 and mins >= 0:
+#     new_hrs = (hrs + delta) % 24
+#     if new_hrs <= 9: new_hrs = '0' + str (new_hrs)
+#     if mins <= 9: mins = '0' + str (mins)
+#     print(f'{new_hrs}:{mins}')
+# else:
+#     print('error')
+
